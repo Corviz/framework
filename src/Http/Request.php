45a7c0a1a6080cdeeaa8bce02915858c1603615a
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2016-06-14 20:10:30 -0300

    Request class basic implement

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -5,9 +5,115 @@ namespace Corviz\Http;
 
 class Request
 {
+
     const METHOD_GET = 'GET';
     const METHOD_POST = 'POST';
     const METHOD_PUT = 'PUT';
     const METHOD_PATCH = 'PATCH';
     const METHOD_DELETE = 'DELETE';
+
+    /**
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * @var array
+     */
+    private $headers = [];
+
+    /**
+     * @var null
+     */
+    private $method = null;
+
+    /**
+     * @var string
+     */
+    private $url = null;
+
+    /**
+     * @return string[]
+     */
+    public static function getValidMethods()
+    {
+        return [
+            self::METHOD_GET,
+            self::METHOD_POST,
+            self::METHOD_PUT,
+            self::METHOD_PATCH,
+            self::METHOD_DELETE
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @return null
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @param array $headers
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @param string $method
+     */
+    public function setMethod(string $method)
+    {
+        $validMethods = self::getValidMethods();
+
+        if(!in_array($method, $validMethods)){
+            throw new \InvalidArgumentException("Invalid method: $method");
+        }
+
+        $this->method = $method;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url)
+    {
+        $this->url = $url;
+    }
+
 }
