bd82cd54337fc60dd996eee60cae19a4224df4a7
xxnoobmanxx <carloscarucce@gmail.com>
2016-06-17 13:14:52 -0300

    Read current request information and generate request representation

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -13,6 +13,21 @@ class Request
     const METHOD_DELETE = 'DELETE';
 
     /**
+     * @var static
+     */
+    private static $currentRequest = null;
+
+    /**
+     * @var boolean
+     */
+    private $ajax = false;
+
+    /**
+     * @var string
+     */
+    private $clientIp = null;
+
+    /**
      * @var array
      */
     private $data = [];
@@ -23,14 +38,47 @@ class Request
     private $headers = [];
 
     /**
-     * @var null
+     * @var string
      */
     private $method = null;
 
     /**
+     * @var string|null
+     */
+    private $rawContent = null;
+
+    /**
      * @var string
      */
-    private $url = null;
+    private $routeStr = null;
+
+    /**
+     * @var boolean
+     */
+    private $secure = false;
+
+    /**
+     * @return Request|static
+     */
+    public static function current()
+    {
+        if(is_null(self::$currentRequest)){
+            $request = new static;
+
+            //fill up complex object properties
+            self::fillCurrentHeaders($request);
+            self::fillCurrentRouteString($request);
+            //TODO Determine if is ajax
+            //TODO Retrieve client IP
+            //TODO Capture rawBody and translate it to data array
+            //TODO Inform method
+            //TODO Determine if its using secure connection (HTTPS)
+
+            self::$currentRequest = $request;
+        }
+
+        return self::$currentRequest;
+    }
 
     /**
      * @return string[]
@@ -44,6 +92,73 @@ class Request
             self::METHOD_PATCH,
             self::METHOD_DELETE
         ];
+    }
+
+    /**
+     * Fill up the headers property with the values
+     * provided by the client browser
+     *
+     * @param Request $request
+     */
+    private static function fillCurrentHeaders(Request $request)
+    {
+        $headers = [];
+
+        //Native method exists?
+        if(function_exists('getallheaders')){
+
+            //try to use it
+            if(($headers = getallheaders()) === false){
+                $headers = [];
+            }
+
+        }
+
+        //No header captured yet? Try to get it myself
+        if(empty($headers)){
+
+            /*
+             * Based on a user note provided by joyview
+             * at http://php.net/manual/en/function.getallheaders.php
+             */
+            foreach($_SERVER as $key => $value){
+                if(substr($key, 0, 5) === 'HTTP_'){
+                    $newKey = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($key, 5)))));
+                    $headers[$newKey] = $value;
+                }
+            }
+
+        }
+
+        $request->setHeaders($headers);
+    }
+
+    /**
+     * Capture the route string
+     * @param Request $request
+     */
+    private static function fillCurrentRouteString(Request $request)
+    {
+        //Read data from $_SERVER array
+        $requestUri = $_SERVER['REQUEST_URI'];
+        $scriptName = $_SERVER['SCRIPT_NAME'];
+
+        //Extract route string from the URI
+        $length = strlen(dirname($scriptName));
+        $route = substr(explode('?', $requestUri)[0], $length);
+
+        //Normalize slashes
+        $route = '/'.trim(str_replace('\\', '/', $route), '/').'/';
+
+        $request->setRouteStr($route);
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientIp()
+    {
+        return $this->clientIp;
     }
 
     /**
@@ -71,11 +186,51 @@ class Request
     }
 
     /**
+     * @return null|string
+     */
+    public function getRawContent()
+    {
+        return $this->rawContent;
+    }
+
+    /**
      * @return string
      */
-    public function getUrl()
+    public function getRouteStr()
     {
-        return $this->url;
+        return $this->routeStr;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isAjax() : boolean
+    {
+        return $this->ajax;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isSecure() : boolean
+    {
+        return $this->secure;
+    }
+
+    /**
+     * @param boolean $ajax
+     */
+    public function setAjax(boolean $ajax)
+    {
+        $this->ajax = $ajax;
+    }
+
+    /**
+     * @param string $clientIp
+     */
+    public function setClientIp(string $clientIp)
+    {
+        $this->clientIp = $clientIp;
     }
 
     /**
@@ -109,11 +264,27 @@ class Request
     }
 
     /**
-     * @param string $url
+     * @param null|string $rawContent
      */
-    public function setUrl(string $url)
+    public function setRawContent(string $rawContent = null)
     {
-        $this->url = $url;
+        $this->rawContent = $rawContent;
+    }
+
+    /**
+     * @param string $routeStr
+     */
+    public function setRouteStr(string $routeStr)
+    {
+        $this->routeStr = $routeStr;
+    }
+
+    /**
+     * @param boolean $secure
+     */
+    public function setSecure(boolean $secure)
+    {
+        $this->secure = $secure;
     }
 
 }
