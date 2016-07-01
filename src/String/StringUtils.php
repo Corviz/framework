af0d8e43925e4c0917c91fb4da20decfdbc6d6a6
xxnoobmanxx <carloscarucce@gmail.com>
2016-07-01 14:16:32 -0300

    StringUtils PHPUnit basic tests for StringUtils

---
+++ src/String/StringUtils.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Corviz\String;
+
+
+class StringUtils
+{
+
+    /**
+     * Checks if $haystack ends with $needle
+     * @param string $haystack
+     * @param string $needle
+     * @param bool $caseSensitive
+     * @return bool
+     */
+    public static function endsWith(
+        string $haystack,
+        string $needle,
+        $caseSensitive = true
+    ) : bool
+    {
+        $needleLength = strlen($needle);
+        $cmp = substr_compare(
+            $haystack,
+            $needle,
+            -($needleLength),
+            $needleLength,
+            !$caseSensitive
+        );
+        return $cmp === 0;
+    }
+
+    /**
+     * Checks if $haystack starts with $needle
+     * @param string $haystack
+     * @param string $needle
+     * @param bool $caseSensitive
+     * @return bool
+     */
+    public static function startsWith(
+        string $haystack,
+        string $needle,
+        $caseSensitive = true
+    ) : bool
+    {
+        $fName = $caseSensitive ? 'strpos' : 'stripos';
+        return $fName($haystack, $needle) === 0;
+    }
+
+}
---
+++ tests/String/StringUtilsTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CorvizTest\String;
+
+
+use Corviz\String\StringUtils;
+
+class StringUtilsTest extends \PHPUnit_Framework_TestCase
+{
+
+    /*
+     * Method: startsWith
+     *------------------------------
+     */
+
+    public function testStartsWithCaseSensitive()
+    {
+        $this->assertTrue(
+            StringUtils::startsWith('abcde', 'a'),
+            'String "abcde" should start with "a" (case sensitive)'
+        );
+    }
+
+    public function testStartsWithCaseInsensitive()
+    {
+        $this->assertTrue(
+            StringUtils::startsWith('abcde', 'A', false),
+            'String "abcde" should start with "A" (case insensitive)'
+        );
+    }
+
+    public function testStartsWithNeedleGreaterThanHaystackShouldFail(){
+        $this->assertFalse(
+            StringUtils::startsWith("a", "abc"),
+            '"a" may not start with "abc"'
+        );
+    }
+
+    /*
+     * Method: endsWith
+     *------------------------------
+     */
+
+    public function testEndsWithCaseSensitive()
+    {
+        $this->assertTrue(
+            StringUtils::endsWith('abcde', 'e'),
+            'String "abcde" should end with "e" (case sensitive)'
+        );
+    }
+
+    public function testEndsWithCaseInsensitive()
+    {
+        $this->assertTrue(
+            StringUtils::endsWith('abcde', 'E', false),
+            'String "abcde" should end with "E" (case insensitive)'
+        );
+    }
+
+    public function testEndsWithNeedleGreaterThanHaystackShouldFail(){
+        $this->assertFalse(
+            StringUtils::endsWith("a", "abc"),
+            '"a" may not end with "abc"'
+        );
+    }
+
+}
