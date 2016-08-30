241e3f3b6f7042ee5a7ff770c6f424a9ab6ba60c
Carlos A B Carucce <carloscarucce@gmail.com>
2016-08-30 14:58:31 -0300

    DI\Container automated test

---
+++ tests/DI/ContainerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Corviz\Framework\DI;
+
+use Corviz\DI\Container;
+
+class ContainerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCallToDeclaredDependency()
+    {
+        $container = new Container();
+        $container->set('DB', MySql::class);
+
+        $this->assertTrue($container->DB instanceof MySql);
+    }
+
+    public function testCallToUndeclaredShouldThrowException()
+    {
+        $this->expectException(\Exception::class);
+        $container = new Container();
+        $container->IWasNotSet->doStuff();
+    }
+
+    public function testChangeContainerDependencyInRuntime()
+    {
+        $container = new Container();
+
+        $container->set('DB', MySql::class);
+        $this->assertTrue($container->DB instanceof MySql, "Expecting MySql instance");
+
+        $container->set('DB', Postgres::class);
+        $this->assertTrue($container->DB instanceof Postgres, "Expecting Postgres instance");
+    }
+}
+
+/*
+ * Stub classes used only for container test
+ */
+
+interface Database
+{
+    public function select(string $columns, string $table);
+}
+
+class MySql implements Database
+{
+    public function select(string $columns, string $table)
+    {
+        return 'mysqlSelect';
+    }
+}
+
+class Postgres implements Database
+{
+    public function select(string $columns, string $table)
+    {
+        return 'postgresSelect';
+    }
+}
+
+class Model
+{
+    private $database;
+
+    /**
+     * @return Database
+     */
+    public function getDatabase(): Database
+    {
+        return $this->database;
+    }
+
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
+    }
+}
