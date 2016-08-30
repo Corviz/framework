3c3d2fdbdce817a2d4b38880435d96d87989f57c
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2016-08-29 21:57:26 -0300

    Controller uses dependency container

--- src/Mvc/Controller.php
+++ src/Mvc/Controller.php
@@ -2,6 +2,35 @@
 
 namespace Corviz\Mvc;
 
+use Corviz\DI\Container;
+
 class Controller
 {
+    /**
+     * @var Container
+     */
+    private $diContainer;
+
+    protected function getContainer() : Container
+    {
+        return $this->diContainer;
+    }
+
+    /**
+     * Controller constructor.
+     *
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->diContainer = $container;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function __get($name)
+    {
+        return $this->diContainer->{$name};
+    }
 }
