078fa31f54ad09b1465a5958d398d631a810cc5b
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2016-08-31 22:10:34 -0300

    Events

---
+++ src/Events/EventHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Corviz\Events;
+
+use Corviz\Behaviour\Observable;
+use Corviz\Behaviour\Observer;
+use Corviz\Behaviour\Runnable;
+
+abstract class EventHandler implements Observer, Runnable
+{
+    /**
+     * @inheritdoc
+     */
+    final public function notify(Observable $observable, ...$args) {
+        if ($observable instanceof Event) {
+            $this->run($observable, ...$args);
+        }
+    }
+
+    abstract public function run(...$args);
+}
