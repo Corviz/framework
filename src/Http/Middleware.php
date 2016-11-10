1d67330ccf1aa95df0998e296f0bd2ea721886ea
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2016-11-10 01:23:24 -0200

    Simplify middleware base class

--- src/Http/Middleware.php
+++ src/Http/Middleware.php
@@ -7,32 +7,9 @@ use Closure;
 abstract class Middleware
 {
     /**
-     * @var Request
-     */
-    private $request;
-
-    /**
      * @param \Closure $next
      *
      * @return void
      */
     abstract public function handle(Closure $next);
-
-    /**
-     * @return \Corviz\Http\Request
-     */
-    public function getRequest() : Request
-    {
-        return $this->request;
-    }
-
-    /**
-     * Middleware constructor.
-     *
-     * @param \Corviz\Http\Request $request
-     */
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
 }
