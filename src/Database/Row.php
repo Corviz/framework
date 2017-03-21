fb8532a13d21698fe0ed521896ffbad66b4717f0
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2017-03-20 23:28:52 -0300

    Database row implements JsonSerializable interface

--- src/Database/Row.php
+++ src/Database/Row.php
@@ -2,12 +2,24 @@
 
 namespace Corviz\Database;
 
-class Row implements \ArrayAccess
+class Row implements \ArrayAccess, \JsonSerializable
 {
     /**
      * @var array
      */
     private $data;
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return ($this->data);
+    }
 
     /**
      * {@inheritdoc}
